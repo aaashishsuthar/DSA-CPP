961cdd32dd23937421c43d1e28a55c5a287bf7dd
Aashish <aksuthar2005@gmail.com>
2026-06-15 22:37:06 +0530

    Day4ofDSA

--- Day3ofDSA.cpp
+++ Day3ofDSA.cpp
@@ -3,11 +3,21 @@ using namespace std;
 int fact(int n){
 int fact = 1;
 for(int i=1; i<=n; i++){
-    fact = fact*i;
+    fact = fact*i;      
 }
 return fact;
 }
+int sum (int a, int b){
+    int sum =0;
+    sum = a+b;
+      return sum;
+}
+int min(int a, int b){
+    return (a<b)? a:b;
+}
 int main(){
-    cout<<"Factorial = " << fact(5);
+    cout<< "Sum = " << sum(2,4) <<endl;
+    cout<< "Minimum = " << min(2,4) <<endl;
+    cout<<"Factorial = " << fact(9) <<endl;
     return 0;
 }
